752678081427f80d41a5c1d094b0bdc52619c2a3
Ken Lin <kel117@pitt.edu>
2020-07-10 19:00:26 -0400

    commit

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI:process.env.MONGO_URI
-}
+  mongoURI:
+    "mongodb+srv://Ken:bLP4qr1ngGSfzHzt@cluster0.jurbb.mongodb.net/movie-app?retryWrites=true&w=majority",
+};
